b8be8cd39725e9b24f48f1afa7b456ac054fb944
Olga-199 <121151860+Olga-199@users.noreply.github.com>
2023-02-12 12:32:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export function multiply(a, b) {
-    return a * b;
+function isPrime(number) {
+  if (number < 1)
+      return "Число не может быть меньше единицы.";
+
+  if (number > 100)
+      return "Число не может быть больше ста.";
+
+  if (!Number.isInteger(number))
+      return "Ожидается целое число.";
+
+  let answer = String();
+
+  for (let i = number; i >= 1; i--)
+      answer += i;
+
+  return answer;
 }
